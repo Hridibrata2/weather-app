dd9ec4104b9c05d7d0f4c7a3099aa9d0ce9c0f51
Hridibrata Saha <hridibrata@gmail.com>
2025-06-25 11:55:07 +0530

    new things workng

--- src/components/AirConditon.jsx
+++ src/components/AirConditon.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { WeatherContext } from './WeatherContext';
 import './AirConditon.css';
 import { CiTempHigh } from "react-icons/ci";
 import { LuWind } from "react-icons/lu";
@@ -7,6 +8,13 @@ import { FaSun } from "react-icons/fa";
 
 
 const AirConditon = () => {
+
+const { weatherData } = useContext(WeatherContext);
+
+const humidityMain = weatherData?.main?.humidity ?? 'N/A';
+const realFeel = Math.round(weatherData?.main?.feels_like - 273.15) ?? 'N/A';
+const windSpeed = weatherData?.wind?.speed ?? 'N/A';
+
   return (
     <div className='air-condition'>
       <p className='condition-header'>AIR CONDITION</p>
@@ -15,13 +23,13 @@ const AirConditon = () => {
         <div className='condition-item'>
           <CiTempHigh className='condition-temp-icon' size={30} />
           <p className='condition-temp-text'>Real Feel</p>
-          <p className='condition-temp-value'>30°</p>
+          <p className='condition-temp-value'>{realFeel}°C</p>
           <LuWind className='condition-wind-icon' size={30} />
           <p className='condition-wind-text'>Wind</p>
-          <p className='condition-wind-value'>10km/h</p>
+          <p className='condition-wind-value'>{windSpeed}km/h</p>
           <IoWaterSharp className='condition-water-icon' size={30} />
           <p className='condition-humidity-text'>Humidity</p>
-          <p className='condition-humidity-value'>60%</p>
+          <p className='condition-humidity-value'>{humidityMain}%</p>
           <FaSun className='condition-sun-icon' size={30} />
           <p className='condition-sun-text'>UV Index</p>
           <p className='condition-sun-value'>3</p>
